372f2d41a8819db2be2c9c83c95c9d11c727c1e6
ArshadTest <155864464+ArshadTest@users.noreply.github.com>
2024-01-09 20:16:28 +0530

    Add files via upload

---
+++ day1homeassignments/FindNumber_IfElse.java
@@ -0,0 +1,16 @@
+package week1.day1homeassignments;
+
+public class FindNumber_IfElse {
+
+	public static void main(String[] args) {
+		int number = 45;
+		
+		if (number>0) {
+			System.out.println("The given number is Positive");
+		} else {
+			System.out.println("The given number is Negative");
+		}
+		
+	}
+
+}
---
+++ day1homeassignments/Firefox_DataTypes.java
@@ -0,0 +1,23 @@
+package week1.day1homeassignments;
+	
+//Data types
+
+public class Firefox_DataTypes {
+
+	public static void main(String[] args) {
+		float browserVersion = 3.03f;
+		String browserName = "Google Chrome";
+		boolean pageLoaded = true;
+		int noOfBrowser = 1;
+		char browserLogo = 'G';
+		
+		System.out.println("The browser version is " +browserVersion);
+		System.out.println("The browser name is " +browserName);
+		System.out.println("Is the page Loaded " +pageLoaded);
+		System.out.println("How many number of browsers " +noOfBrowser);
+		System.out.println("The browser Logo is " +browserLogo);
+
+	
+	}
+
+}
