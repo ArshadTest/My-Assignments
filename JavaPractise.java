212517fd3e01efc2eadf46d8800171b3ac964361
ArshadTest <155864464+ArshadTest@users.noreply.github.com>
2024-11-26 10:48:38 +0530

    Day1
    Day 1 - Explanation

---
+++ JavaPractise.java
@@ -0,0 +1,13 @@
+package javaSelenium;
+
+public class JavaPractise {
+
+ public static void main(String[] args) {
+	 int employeeID = 12454;
+	 String name = "Arshad";
+	 char initial = 'A';
+	 
+	 System.out.println("My employee ID is "+employeeID+ "\n" +"My name is "+name+ "\n" + "My initial is "+initial);
+}
+
+}
---
+++ LearnIfElse.java
@@ -0,0 +1,32 @@
+package javaSelenium;
+
+public class LearnIfElse {
+
+	public static void main(String[] args) {
+		
+		int profession = 2024;
+		
+		if (profession==2024) {
+			System.out.println("Arshad is an Automation Engineer");
+			
+		} else {
+			System.out.println("Arshad should master Automation concepts quickly");
+		}
+// -----------------------------------------------------------------------------------------------------
+		//else if
+		int num = 2035;
+				
+		if (num<=2030) {
+			System.out.println("This is less than "+num);
+		} 
+		else if (num==2035) {
+		System.out.println("The numbers are same");	
+		
+		}
+		else {
+			System.out.println("This is greater than "+num);
+		}
+
+	}
+
+}
